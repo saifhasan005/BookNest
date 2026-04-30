87560302a8d5542247d75c6ece616bf7f32cf60e
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-04-30 23:33:59 +0600

    finished

--- src/app/Components/Books.jsx
+++ src/app/Components/Books.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BookCard from './BookCard';
 
 const Books = async () => {
-    const res = await fetch('http://localhost:3000/data.json');
+    const res = await fetch('http://localhost:3000/data.json',{cache:'no-store'});
     const books = await res.json();
     const topBooks = books.slice(0, 4);
     return (
