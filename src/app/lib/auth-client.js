07b927c3c34b5b23ddf3b4281e6c11fac26c0648
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-05-02 23:18:34 +0600

    added

--- src/app/lib/auth-client.js
+++ src/app/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://book-nest-rho-five.vercel.app/register"
+    baseURL: "https://book-nest-rho-five.vercel.app"
 })
 export const { signIn, signUp, useSession } = createAuthClient()
